7cfee3a314d8d940a504e848e50c7296a30aad85
jasmo2 <jasmo2.com@gmail.com>
2016-08-11 16:56:20 -0500

    delete and update User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  devise :omniauthable, :omniauth_providers => [:facebook]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   belongs_to :tipo_de_documento
   belongs_to :rol
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
 end
--- app/models/usuario.rb
+++
@@ -1,6 +0,0 @@
-class Usuario < ApplicationRecord
-  belongs_to :tipo_de_documento
-  belongs_to :rol
-  has_secure_password
-  validates :nombre_usuario, presence: true, length: { minimum: 10, maximum: 120}
-end
