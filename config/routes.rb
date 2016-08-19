63171ce29ad41cee2afdbbaee174df51dd620e67
jasmo2 <jasmo2.com@gmail.com>
2016-08-19 15:10:51 -0500

    first step routes refactor

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   resources :usuarios
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # :omniauth_callbacks "users/omniauth_callbacks"
 end
