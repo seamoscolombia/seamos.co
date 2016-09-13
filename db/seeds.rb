519e841339344f69a01879ed1e9baa7da80fd85b
jasmo2 <jasmo2.com@gmail.com>
2016-09-13 15:31:25 -0500

    add seeds votes and poll

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,36 @@ Role.create!(code: 'administrador')
 VoteType.create!(code: "yes")
 VoteType.create!(code: "no")
 VoteType.create!(code: "blank")
+
+
+user = Usuario.create!(primer_apellido: "Molina", segundo_apellido: "Orbe", nombres: "Jaime", tipo_de_documento_id: 1, numero_documento: "1113645710", fecha_expedicion: "2011-05-28", role_id: 3, uid: "10154399579960489", valido: false)
+
+poll = Poll.create!(
+    title: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph,
+    private: false,
+    closing_date: 1.day.from_now,
+    usuario_id: user.id
+)
+prng = Random.new
+
+400.times do
+  u = Usuario.create!(
+      primer_apellido: Faker::Lorem.word,
+      segundo_apellido: Faker::Lorem.word,
+      nombres: Faker::Lorem.word,
+      tipo_de_documento_id: 1,
+      numero_documento: Faker::Number.number(10).to_s,
+      fecha_expedicion: Faker::Time.between(3.years.ago, 1.year.ago),
+      role_id: 1,
+      uid: Faker::Number.number(10).to_s ,
+      valido: false
+  )
+  
+
+  vote = Vote.create!(
+    poll: poll,
+    usuario: u,
+    vote_type_id: prng.rand(1..3)
+  )
+end
