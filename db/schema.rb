031881ff16911ccc2493eb89b12b519ae53d7f82
jasmo2 <jasmo2.com@gmail.com>
2016-09-21 16:18:23 -0500

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -61,34 +61,6 @@ ActiveRecord::Schema.define(version: 20160904222923) do
     t.index ["codigo"], name: "index_tipo_de_documentos_on_codigo", unique: true
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "primer_apellido"
-    t.string   "segundo_apellido"
-    t.string   "nombres"
-    t.integer  "tipo_de_documento_id"
-    t.string   "numero_documento"
-    t.string   "provider"
-    t.string   "uid"
-    t.date     "fecha_expedicion"
-    t.integer  "rol_id"
-    t.boolean  "valido",                 default: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["numero_documento"], name: "index_users_on_numero_documento"
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["rol_id"], name: "index_users_on_rol_id"
-    t.index ["tipo_de_documento_id"], name: "index_users_on_tipo_de_documento_id"
-  end
-
   create_table "usuarios", force: :cascade do |t|
     t.string   "primer_apellido"
     t.string   "segundo_apellido"
