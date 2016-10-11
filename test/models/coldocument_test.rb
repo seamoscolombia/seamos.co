436cff37258e2b7b6f1bfc3f8e47e81dfdde63b7
jasmo2 <jasmo2.com@gmail.com>
2016-10-11 12:25:57 -0500

    add test models for coldocuments
    Former-commit-id: dbdbf030c437203ef816b34b320bc2a8c49f45f3

---
+++ test/models/coldocument_test.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: coldocuments
+#
+#  id         :integer          not null, primary key
+#  doc_num    :integer
+#  divipol_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+require 'test_helper'
+
+class ColdocumentTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
