126005cd843493a59a6a4671ef0356cc3d28e5a9
jasmo2 <jasmo2.com@gmail.com>
2016-10-19 22:03:46 -0500

    refactor and validate if functions extists
    Former-commit-id: af6c16d6d83590694ade0bf449591132dcebd5ca

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,4 +1,16 @@
 $( document ).on('turbolinks:load', function() {
-  $(".button-sender").click(formConfig);
-  $("#polls_filter").change(filterPoll);
+  var pF = $("#polls_filter");
+  var bS = $(".button-sender");
+  var bVTA = $("#button_vote_types_attributes");
+
+  if(pF.length > 0){
+    pF.change(filterPoll);
+  }
+  // .button-sender only renders in partial _poll.html.erb
+  if(bS.length > 0){
+    bS.click(formConfig);
+  }
+  if(bVTA.length > 0){
+    bVTA.click(addOption);
+  }
 });
