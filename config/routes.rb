5671b106ad51d5fc72502dabaf2acb47dd38540b
jasmo2 <jasmo2.com@gmail.com>
2016-10-19 22:58:03 -0500

    set route for destroy session
    Former-commit-id: 1a618b5ec461bcc14f14bde41ff9bbee13aa84d3

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   #ToDo ajax call on session
   get '/auth/:provider/callback', to: 'sessions#create'
+  delete '/sessions', to: 'sessions#destroy', as: 'session'
   post '/sessions', to: 'sessions#create', format: 'json'
   get '/auth/sessions', to: 'sessions#error'
 
