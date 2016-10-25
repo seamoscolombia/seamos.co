2213f5daaa25eda8283ded276cf10368b18ce259
jasmo2 <jasmo2.com@gmail.com>
2016-10-25 10:37:09 -0500

    refactor migration
    Former-commit-id: bf888c13f54ef348f0c93446c30af41242e2a464

--- db/migrate/20160904230421_create_vote_types.rb
+++ db/migrate/20160904230421_create_vote_types.rb
@@ -1,6 +1,6 @@
 class CreateVoteTypes < ActiveRecord::Migration[5.0]
   def change
-    create_table :vote_types do |t|
+    create_table :vote_types, force: true do |t|
       t.string :name, null: false
       t.references :poll, foreign_key: true
       t.timestamps
