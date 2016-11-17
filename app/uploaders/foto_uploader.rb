d598f219f7c89d9572229a5feab724aa8ad1d730
jasmo2 <jasmo2.com@gmail.com>
2016-11-17 12:05:44 -0500

    condigure uploader
    Former-commit-id: a9018268f7233dbc57017dde7b75bca57b1b17e9

--- app/uploaders/foto_uploader.rb
+++ app/uploaders/foto_uploader.rb
@@ -47,8 +47,8 @@ class FotoUploader < CarrierWave::Uploader::Base
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+    "photo.png"
+  end
 
 end
