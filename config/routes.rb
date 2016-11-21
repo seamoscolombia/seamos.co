7e27c4bf2add48cd4f6d05c2968b6f1f73aabdde
jasmo2 <jasmo2.com@gmail.com>
2016-11-21 11:08:43 -0500

    add all resources to Usuario
    Former-commit-id: 6fd77bff61d2f44c3ed20c058603fa2ab94ae52b

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'intro#inicio'
 
   resources :photos, only: :create
-  resources :usuarios, only: [:index,:new, :create, :edit]
+  resources :usuarios 
 
   #ToDo ajax call on session
   get '/auth/:provider/callback', to: 'sessions#create'
