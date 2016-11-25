0a42c1915344844051c5cbf26641a8b75f450eab
jasmo2 <jasmo2.com@gmail.com>
2016-11-25 11:19:40 -0500

    remove unnecesary routes
    Former-commit-id: 4800eaa7adaf25740c172faea6c11c95828db214

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'intro#inicio'
 
   resources :photos, only: :create
-  resources :usuarios 
+  resources :usuarios , except: [:new, :show]
 
   #ToDo ajax call on session
   get '/auth/:provider/callback', to: 'sessions#create'
