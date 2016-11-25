137cc139f5d33e36a7697c112a823af909931c0f
jasmo2 <jasmo2.com@gmail.com>
2016-11-25 11:18:07 -0500

    add usuarios_controller_test
    Former-commit-id: 9baf1d3bb332b33d4a5e34da854ac5ec68a9a041

--- test/controllers/usuarios_controller_test.rb
+++ test/controllers/usuarios_controller_test.rb
@@ -5,40 +5,69 @@ class UsuariosControllerTest < ActionDispatch::IntegrationTest
     @usuario = usuarios(:one)
   end
 
-  test "should get index" do
+  test "should get non-valid 'usuarios'" do
+    init_session_json("uidString3","fb_token")
     get usuarios_url
-    assert_response :success
+
+    assert_select 'h1', 'Usuarios'
+    assert_select 'li', count: 2
   end
 
-  test "should get new" do
-    get new_usuario_url
-    assert_response :success
+
+  test "should create usuario with format_json" do
+    init_session_json("uid","fb_token")
+    assert_difference('Usuario.count') do
+       post usuarios_url, params: {
+          photo_id: document_photos(:one).id,
+          usuario: {
+            fecha_expedicion: @usuario.fecha_expedicion,
+            nombres: "three", numero_documento: 1_113_645_711.to_s,
+            primer_apellido: "third surname",
+            segundo_apellido: "third second surname",
+            tipo_de_documento_id: tipo_de_documentos(:one).id
+         } }, as: :json
+    end
+    assert_response :created
   end
 
-  # test "should create usuario" do
-  #    assert_difference('Usuario.count') do
-  #      post usuarios_url, params: { usuario: { uid:  @usuario.uid ,fecha_expedicion: @usuario.fecha_expedicion,  nombres: @usuario.nombres, numero_documento: @usuario.numero_documento,  primer_apellido: @usuario.primer_apellido, rol_id: @usuario.rol_id, segundo_apellido: @usuario.segundo_apellido, tipo_de_documento_id: @usuario.tipo_de_documento_id, valido: @usuario.valido } }
-  #    end
-  #
-  #   assert_redirected_to usuario_url(Usuario.last)
+  # test "should create usuario for web" do
+  #   post sessions_url, env: { 'omniauth.auth' => init_omniAuth() }, as:
+  #   assert_difference('Usuario.count') do
+  #      post usuarios_url, params: {
+  #         photo_id: document_photos(:one).id,
+  #         usuario: {
+  #           fecha_expedicion: @usuario.fecha_expedicion,
+  #           nombres: "three", numero_documento: 1_113_645_710.to_s,
+  #           primer_apellido: "third surname",
+  #           segundo_apellido: "third second surname",
+  #           tipo_de_documento_id: tipo_de_documentos(:one).id
+  #        } }
+  #   end
+  #   assert_response :created
   # end
 
-  test "should show usuario" do
-    get usuario_url(@usuario)
-    assert_response :success
-  end
 
   test "should get edit" do
-    get edit_usuario_url(@usuario)
+    init_session_json("uidString3","fb_token")
+    get edit_usuario_url(usuarios(:three))
+    assert_select 'th', count: 5
+    assert_select 'td', text: usuarios(:three).numero_documento, count: 1
     assert_response :success
   end
 
-
-  test "should destroy usuario" do
-    assert_difference('Usuario.count', -1) do
-      delete usuario_url(@usuario)
-    end
-
-    assert_redirected_to usuarios_url
+  test "validating usuario" do
+    init_session_json("uidString3","fb_token")
+    put usuario_url(usuarios(:one).id)
+    assert_equal Usuario.where(valido: false).count, 1
+    # assert_redirected_to usuarios_url
   end
+
+
+  # test "should destroy usuario" do
+  #   assert_difference('Usuario.count', -1) do
+  #     delete usuario_url(@usuario)
+  #   end
+  #
+  #   assert_redirected_to usuarios_url
+  # end
 end
