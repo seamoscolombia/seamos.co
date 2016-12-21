155008f121d2bb17a6b3a5a77b2a128e39013c7f
Carlos Quevedo <carlos.quevedo@pinbit.co>
2016-12-21 10:42:07 -0500

    No clue yet about error posting to FB
    Former-commit-id: 687a8dfc6c089f5a6a8fea259e35985460616f94

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -92,7 +92,9 @@ class PollsController < ApplicationController
       user_graph = Koala::Facebook::API.new(session[:fb_token])
 
       tvtd_page_token = user_graph.get_page_access_token("#{Rails.application.secrets.tvtd_page_id}")
+      logger.debug "Token in publish_facebook: #{tvtd_page_token} "
       page_graph = Koala::Facebook::API.new(tvtd_page_token)
+      logger.debug "page_graph in publish_facebook: #{page_graph} "
 
       page_graph.put_connections(
         "#{Rails.application.secrets.tvtd_page_id}",
