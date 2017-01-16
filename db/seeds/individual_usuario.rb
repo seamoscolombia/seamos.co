63f65f57749e254da3fa3347fbc1756b1fc20933
jasmo2 <jasmo2.com@gmail.com>
2017-01-16 11:57:12 -0500

    refactor individual_usuario.rb seed
    Former-commit-id: 6fafd053a2dba8c4503279c75dae657d26d554fc

--- db/seeds/individual_usuario.rb
+++ db/seeds/individual_usuario.rb
@@ -4,7 +4,7 @@ new_coldoc = Coldocument.create!(doc_num: coldoc.doc_num + 1,divipol_id: coldoc.
 document_photo = DocumentPhoto.create!(:url => "/uploads/document_photo/url/25/photo.png")
 
 usuario_last = Usuario.last
-Usuario.create!(
+usuario = Usuario.create!(
   primer_apellido: "firstSurname",
   segundo_apellido: "secondSurname",
   nombres: "Names",
@@ -16,3 +16,19 @@ Usuario.create!(
   valido: false,
   document_photo_id: document_photo.id
 )
+
+poll_last = Poll.last
+vote_type = poll_last.vote_types.first
+
+if poll_last.private?
+  totals = eval(@poll.totals)
+  totals[vote_type.id] += 1
+  poll_last.totals = totals
+  poll_last.save!
+  vote = usuario.votes.build( poll_id: @poll.id )
+else
+  vote = usuario.votes.build(
+      poll_id: poll_last.id,
+      vote_type: vote_type
+  )
+end
