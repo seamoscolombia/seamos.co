8ae7509a40f26a10d1844cc859c067ffa6227f3b
jasmo2 <jasmo2.com@gmail.com>
2017-01-17 15:52:33 -0500

    refactor
    Former-commit-id: 12a915f3029b305b3efa8b4fcd57fee939d53748

--- app/models/vote_type.rb
+++ app/models/vote_type.rb
@@ -12,5 +12,5 @@
 class VoteType < ApplicationRecord
   validates :name, presence: true
   belongs_to :poll, inverse_of: :vote_types
- validates_presence_of :poll
+  validates_presence_of :poll
 end
