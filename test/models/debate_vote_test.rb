0cf2177d1fb75d53324cf0cf960c8f6bde5a2e9d
jasmo2 <jasmo2.com@gmail.com>
2017-01-18 16:02:04 -0500

    test boiler plate
    Former-commit-id: ef5a0dc1f265fb126dbb07102be68bc0e641c9f8

--- test/models/debate_vote_test.rb
+++ test/models/debate_vote_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: debate_votes
+#
+#  id          :integer          not null, primary key
+#  question_id :integer
+#  usuario_id  :integer
+#  debate_id   :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class DebateVoteTest < ActiveSupport::TestCase
