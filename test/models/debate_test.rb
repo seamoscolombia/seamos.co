dfef60a2c530a2c9ea42bb7c103564e8519d15bc
jasmo2 <jasmo2.com@gmail.com>
2017-01-18 15:37:35 -0500

    add test boiler_plate
    Former-commit-id: 2d84363322a4abe92a57bd910170c6be68b95d1f

--- test/models/debate_test.rb
+++ test/models/debate_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: debates
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  published  :boolean          default(FALSE)
+#  poll_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class DebateTest < ActiveSupport::TestCase
---
+++ test/models/debate_vote_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DebateVoteTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
