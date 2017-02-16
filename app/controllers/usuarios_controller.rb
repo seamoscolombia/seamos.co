541be8c3891cbfed1db6227dd21afbc7fd9ee776
jasmo2 <jasmo2.com@gmail.com>
2017-02-16 11:06:07 -0500

    change bad order
    Former-commit-id: 1b37c7422449452a26f1b9904b6ab072c177525b

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -29,11 +29,11 @@ class UsuariosController < ApplicationController
 
       format.json do
         # server Date format 2015-05-28 YYYY-MM-DD
+        @usuario.fecha_expedicion = fecha_expedicion
         Rails.logger.debug("@usuario.fecha_expedicion.to_s #{@usuario.fecha_expedicion.to_s}")
         Rails.logger.debug("Time.parse(@usuario.fecha_expedicion.to_s #{Time.parse(@usuario.fecha_expedicion.to_s)}")
         Rails.logger.debug("Time.parse(@usuario.fecha_expedicion.to_s).strftime('%Y-%m-%d') #{Time.parse(@usuario.fecha_expedicion.to_s).strftime('%Y-%m-%d')}")
         fecha_expedicion = Time.parse(@usuario.fecha_expedicion.to_s).strftime("%Y-%m-%d")
-        @usuario.fecha_expedicion = fecha_expedicion
         if @usuario.save
           render :show, status: :created, location: usuarios_url(@usuario)
         else
