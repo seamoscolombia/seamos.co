ec6cb79ce82db929676fc20d8a3936d4394f55cb
jasmo2 <jasmo2.com@gmail.com>
2017-03-15 15:48:22 -0500

    add dependent destroy
    Former-commit-id: dbbadb1cc207ef698079eaac46485bb47be7200c

--- app/models/debate.rb
+++ app/models/debate.rb
@@ -14,8 +14,8 @@
 class Debate < ApplicationRecord
   belongs_to :usuario
   belongs_to :poll
-  has_many :questions, inverse_of: :debate
-  has_many :debate_votes, inverse_of: :debate
+  has_many :questions, inverse_of: :debate, dependent: :destroy
+  has_many :debate_votes, inverse_of: :debate, dependent: :destroy
   accepts_nested_attributes_for :questions
 
   validates :questions, length: { minimum: 2,  maximum: 4 }
