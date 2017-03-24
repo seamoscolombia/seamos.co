e3a5f2b9d5f973cad5938fb6c9d0726642d55f4b
Alexander Quiceno <yonnyquiceno@gmail.com>
2017-03-24 12:34:57 -0500

    Add poll_with_votes factory
    Former-commit-id: 77fe0797804e04562695a39d47a181d388659561

--- spec/factories/polls.rb
+++ spec/factories/polls.rb
@@ -1,17 +1,17 @@
 FactoryGirl.define do
-#        poll_image: string)
-
   factory :poll do
     title { Faker::Lorem.sentence }
     description { Faker::Lorem.paragraph }
     closing_date { Date.tomorrow }
     totals "{1=>#{Faker::Number.number(3)}, 2=>#{Faker::Number.number(3)}}"
-    to_create {|instance| instance.save(validate: false) }
+    to_create { |instance| instance.save(validate: false) }
 
-
-    # factory :poll_with_votes do
-    #   after(:create) do |poll|
-    #   end
-    # end
+    factory :poll_with_votes do
+      after(:create) do |poll|
+        5.times do
+          poll.votes << FactoryGirl.create(:vote)
+        end
+      end
+    end
   end
 end
