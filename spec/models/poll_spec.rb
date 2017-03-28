0ed3f624ae200656f799da2979fe30c2f3c0a571
Alexander Quiceno <yonnyquiceno@gmail.com>
2017-03-27 20:54:01 -0500

    Add Spec coverage for Poll model
    Former-commit-id: 658f51bfe695921a957ab1ee0b77586f83e4037b

---
+++ spec/models/poll_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+RSpec.describe Poll, type: :model do
+  describe 'relations' do
+    it { should belong_to(:usuario) }
+    it { should have_many(:vote_types) }
+    it { should have_many(:votes) }
+    it { should have_many(:debates) }
+    it { should accept_nested_attributes_for(:vote_types) }
+  end
+
+
+  describe 'validations' do
+    let(:poll) { Poll.new(closing_date: Date.today - 3.days) }
+    it 'should validate closing_date' do
+      poll.valid?
+      expect(poll.errors[:closing_date]).to include('Fecha Inválida')
+    end
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:closing_date) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:poll_image) }
+  end
+
+  describe 'poll#closed?' do
+    let(:poll) { FactoryGirl.build(:poll, closing_date: Date.today - 3.days) }
+    context 'when poll closing date has passed' do
+      it 'returns true' do
+        expect(poll.closed?).to eq(true)
+      end
+    end
+    context 'when poll closing date has not passed yet' do
+      it 'returns false' do
+        poll.update(closing_date: Date.today + 3.days)
+        expect(poll.closed?).to eq(false)
+      end
+    end
+  end
+
+  describe 'poll#published_debates?' do
+    let(:poll) { FactoryGirl.create(:poll_with_votes_and_debates) }
+    context 'when poll has not published debates' do
+      it 'returns an empty array' do
+        expect(poll.published_debates).to eq([])
+      end
+    end
+    context 'when poll has published debates' do
+      it 'returns only publshed debates' do
+        debates = poll.debates
+        debates.first.update(published: true)
+        debates.last.update(published: true)
+        expect(poll.published_debates).to include(debates.first, debates.last)
+      end
+    end
+  end
+end
