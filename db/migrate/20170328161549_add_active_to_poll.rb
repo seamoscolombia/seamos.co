25bb08caaabee801051cef3645676adb68d5dcff
jasmo2 <jasmo2.com@gmail.com>
2017-03-28 13:20:02 -0500

    should be true
    Former-commit-id: c8b3881223f34e966e2dce635cc7eaad35442baf

--- db/migrate/20170328161549_add_active_to_poll.rb
+++ db/migrate/20170328161549_add_active_to_poll.rb
@@ -1,5 +1,5 @@
 class AddActiveToPoll < ActiveRecord::Migration[5.0]
   def change
-    add_column :polls, :active, :boolean, default: false
+    add_column :polls, :active, :boolean, default: true
   end
 end
