46d87661980887bb5cb56744b2a14a4d5f7cc216
jasmo2 <jasmo2.com@gmail.com>
2017-04-03 16:24:10 -0500

    Add counter JS
    Former-commit-id: db70ca84bec428e225c34e7480cae6314b8f6fe5

--- app/assets/javascripts/polls.js
+++ app/assets/javascripts/polls.js
@@ -1,5 +1,6 @@
 
-$( document ).on('turbolinks:load', function() {
+$(document).on('turbolinks:load', function() {
+
   function csvField() {
     if (pollPrivateTrue.checked) {
       $('#csv-field').show();
@@ -7,10 +8,18 @@ $( document ).on('turbolinks:load', function() {
       $('#csv-field').hide();
     }
   }
+  function charLeft (){
+    counterLabel.text(500 - (this.value.length));
+  }
+  var textArea = $('#poll-description');
+  var counterLabel = $('#counter-label');
   var pollPrivateTrue = document.getElementById('poll_private_true');
   var pollPrivateFalse = document.getElementById('poll_private_false');
+
   if (pollPrivateTrue && pollPrivateFalse) {
     pollPrivateTrue.addEventListener('click', csvField, false);
     pollPrivateFalse.addEventListener('click', csvField, false);
+    counterLabel.text(500 - (textArea.val().length));
+    textArea.on('keydown', charLeft);
   }
 });
