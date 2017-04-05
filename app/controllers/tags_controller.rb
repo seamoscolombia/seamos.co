cb1d2e1969de12545e6eb39058f76abec08f062d
Alexander Quiceno <yonnyquiceno@gmail.com>
2017-04-05 16:42:04 -0500

    Validate superadmin before tag create
    Former-commit-id: 9e5e045646c683ecd089b5d770d713b495383ae8

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,8 @@
 class TagsController < ApplicationController
   before_action :set_tags, only: :create
   before_action :tag_params, only: :set_tags
-  before_action :existing_tags, only: [:create, :new]
+  before_action :existing_tags, only: %i(create new)
+  before_action :validate_superadmin, only: %i(create new)
 
   def new
     @tag = Tag.new
@@ -14,16 +15,14 @@ class TagsController < ApplicationController
       if @existing_tags.include? name.delete(' ')
         @already_created_tags += 1
       else
-      @tag = Tag.new(name: name)
-         if @tag.save
-           @existing_tags.push(name)
-           @created_tags += 1
-         end
+        @tag = Tag.new(name: name)
+        if @tag.save
+          @existing_tags.push(name)
+          @created_tags += 1
+        end
        end
     end
-    if @created_tags + @already_created_tags == @tags.count
-      redirect_to :back
-    end
+    redirect_to :back if @created_tags + @already_created_tags == @tags.count
   end
 
   private
@@ -38,5 +37,9 @@ class TagsController < ApplicationController
 
   def tag_params
     params.require(:tag).permit(:name)
+  end
+
+  def validate_superadmin
+    redirect_to :root if current_user.role_type != "administrador"
   end
 end
