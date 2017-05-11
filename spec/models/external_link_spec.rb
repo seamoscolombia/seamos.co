b107e5a505459673c1ad6f306cd6f842d992ea2f
Alexander Quiceno <yonnyquiceno@gmail.com>
2017-05-11 18:03:57 -0500

    Fix failing test
    Former-commit-id: 26d15290a7193ccb943385c92dbbd64bfe3d0548

--- spec/models/external_link_spec.rb
+++ spec/models/external_link_spec.rb
@@ -12,23 +12,21 @@
 require 'rails_helper'
 
 RSpec.describe ExternalLink, type: :model do
-    # describe 'relations' do
-    #   it { should belong_to(:poll) }
-    # end
+    describe 'relations' do
+      it { should belong_to(:poll) }
+    end
 
     describe 'validations' do
       let(:poll) { FactoryGirl.create(:poll) }
-      context 'custom' do
-        let(:external_link) { FactoryGirl.create(:external_link) }
-        it 'url field has invalid URL' do
-          external_link.url = 'not a url'
-          byebug
+      context 'custom url field' do
+        let(:external_link) { FactoryGirl.build(:external_link) }
+        it 'has invalid URL' do
+          external_link.url = 'not an url'
           external_link.poll = poll
           external_link.valid?
           expect(external_link.errors[:url]).to include('es inválido')
         end
-       
-        it 'url field has valid URL' do
+        it 'has valid URL' do
           external_link.poll = poll
           expect(external_link.valid?).to be true
         end
@@ -39,7 +37,7 @@ RSpec.describe ExternalLink, type: :model do
       it 'user reference field is invalid' do
         external_link.poll = nil
         external_link.valid?
-        expect(external_link.errors[:user]).to include('User es requerido')
+        expect(external_link.errors[:poll]).to include('Propuesta es requerido')
       end
     end
 end
