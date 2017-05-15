074562a50566e55e812a8b8d8b13f23bd066c6cb
Alexander Quiceno <yonnyquiceno@gmail.com>
2017-05-15 14:31:16 -0500

    Ad tag image field, refactor tags list, add update and edit methods, add tags edit view
    Former-commit-id: e19a054b8c89724783a1db69cdefa8e5b119f31a

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,6 @@
 class TagsController < ApplicationController
   before_action :tag_params, only: :set_tags
-  before_action :validate_superadmin, only: %i(create new delete)
+  before_action :validate_superadmin, only: %i(create new delete edit)
 
   def new
     @tag = Tag.new
@@ -20,12 +20,27 @@ class TagsController < ApplicationController
 
   def create
     @tag = Tag.new(tag_params)
-    if @tag.save 
-      flash[:success] = I18n.t(:tags_creados, created: "#{@tags.to_s} etiquetas") 
+    if @tag.save
+      flash[:success] = I18n.t(:tags_creados, created: "#{@tags.to_s} etiquetas")
+      redirect_to admin_tags_new_path and return
     else
       logger.debug "ERROR TAG CREATION: #{@tag.errors.messages}"
+      render :new and return
     end
-    redirect_back(fallback_location: root_path)
+  end
+
+  def edit
+    @tag = Tag.find(params[:id])
+  end
+
+  def update
+    @tag = Tag.find(params[:id])
+    if @tag.update(tag_params)
+      flash[:success] = I18n.t(:accion_exitosa)
+      redirect_to admin_tags_new_path
+    else
+      render :edit
+    end
   end
 
   def delete
@@ -37,7 +52,7 @@ class TagsController < ApplicationController
   private
 
   def tag_params
-    params.require(:tag).permit(:name, :image_tag)
+    params.require(:tag).permit(:name, :tag_image)
   end
 
   def validate_superadmin
