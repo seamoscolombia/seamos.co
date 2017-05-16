e3563ed020d611482a6c1b611381d56400356bdf
Alexander Quiceno <yonnyquiceno@gmail.com>
2017-05-16 12:05:35 -0500

    Add spec coverage for poll controller
    Former-commit-id: ac00a5c63ab924a0659b5ecc9d197ddf6c26feca

--- spec/controllers/polls_controller_spec.rb
+++ spec/controllers/polls_controller_spec.rb
@@ -42,4 +42,23 @@ RSpec.describe PollsController, type: :controller do
       expect(response).to have_http_status(:ok)
     end
   end
+  describe 'GET edit' do
+    context 'when the user is not an admin' do
+      let(:poll) { FactoryGirl.create(:poll) }
+      it 'assigns redirects to root path' do
+        get :edit, params: {id: poll.id}
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context 'when the user is an admin' do
+      let(:poll) { FactoryGirl.create(:poll) }
+      let(:user) { FactoryGirl.create(:user, role_type: 2)}
+      it 'renders the edit template' do
+        session[:email] = user.email
+        get :edit, params: {id: poll.id}
+        expect(response).to render_template(:edit)
+      end
+    end
+  end
 end
