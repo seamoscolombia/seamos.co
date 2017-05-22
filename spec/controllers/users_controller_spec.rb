764a10289cf19f65da428e768b5bb683de1a024a
Alexander Quiceno <yonnyquiceno@gmail.com>
2017-05-22 13:51:12 -0500

    Add spec coverage for users controller
    Former-commit-id: 7480b4bf2044e525ddb24a96ad77f86008a2cdea

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,37 +4,55 @@ include SessionsHelper
 RSpec.describe UsersController, type: :controller do
   describe 'GET index' do
     context 'when the request format is HTML' do
+
       it 'get users' do
         5.times { FactoryGirl.create(:user) }
         get :index
         expect(response).to have_http_status(:found)
       end
+      
     end
   end
 
   describe 'GET show' do
    let(:user) { FactoryGirl.create(:user)}
    context 'when the request format is JSON' do
-     it 'assigns @user' do
-       get :show, params: { id: user.id }, format: :json
-       expect(assigns(:user)).to eq(user)
-     end
+     context 'and the user is not logged in' do
 
-     it 'renders the show template' do
-       get :show, params: { id: user.id }, format: :json
-       expect(response).to render_template('show')
-     end
+       it 'renders an error' do
+         get :show, format: :json
+         expect(JSON.parse(response.body)["errors"]).to eq("El usuario debe iniciar sesión para ver su informacion de perfil")
+       end
 
-     it 'should return status ok' do
-       get :show, params: { id: user.id }, format: :json
-       expect(response).to have_http_status(:ok)
+     end
+     context 'and the user is logged in' do
+
+       before(:each) do
+         session[:email] = user.email
+       end
+
+       it 'assigns @user' do
+         get :show, params: { id: user.id }, format: :json
+         expect(assigns(:user)).to eq(user)
+       end
+
+       it 'renders the show template' do
+         get :show, params: { id: user.id }, format: :json
+         expect(response).to render_template('show')
+       end
+
+       it 'should return status ok' do
+         get :show, params: { id: user.id }, format: :json
+         expect(response).to have_http_status(:ok)
+       end
+
      end
    end
 
    describe "responds to" do
-     it "responds to html by default" do
+     it "responds to JSON by default" do
        get :show, params: { id: user.id }
-       expect(response.content_type).to eq "text/html"
+       expect(response.content_type).to eq "application/json"
      end
 
      it "responds to custom formats when provided in the params" do
