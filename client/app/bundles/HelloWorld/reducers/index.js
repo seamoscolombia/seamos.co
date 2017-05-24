63398fcaee69b39bf0b0d31839f07e231342c6a7
jasmo2 <jasmo2.com@gmail.com>
2017-05-24 16:22:23 -0500

    refactr polls reducer
    Former-commit-id: 823b0e03ccaf81cb06dea64a32274115954e1c39

--- client/app/bundles/HelloWorld/reducers/index.js
+++ client/app/bundles/HelloWorld/reducers/index.js
@@ -1,16 +1,12 @@
 import { combineReducers } from 'redux';
 import PollReducer from './pollReducer';
 import PollsReducer from './pollsReducer';
-import VotedDebateReducer from './votedDebateReducer';
 import VotedPollReducer from './votedPollReducer';
 import VotedPollsReducer from './votedPollsReducer';
-import SwiperReducer from './swiperReducer';
 
 export default combineReducers({
   actualPoll: PollReducer,
-  actualVotedDebate: VotedDebateReducer,
   actualVotedPoll: VotedPollReducer,
   polls: PollsReducer,
   votedPolls: VotedPollsReducer,
-  swiper: SwiperReducer,
 });
--- client/app/bundles/HelloWorld/reducers/pollsReducer.js
+++ client/app/bundles/HelloWorld/reducers/pollsReducer.js
@@ -1,10 +1,8 @@
 import {
-  INDEX_POLLS,
   RESULT_POLLS,
   UPDATE_POLLS
 } from '../constants';
 
-import { TVTDURL } from '../constants';
 
 // const INITIAL_STATE = [{ //use for reference
 //   id: null,
@@ -17,10 +15,6 @@ import { TVTDURL } from '../constants';
 
 export default (state = [], action) => {
   switch (action.type) {
-    case INDEX_POLLS:
-      return action.polls;
-    case RESULT_POLLS:
-      return { ...state, url: `${TVTDURL}polls/voted` };
     case UPDATE_POLLS:
       return Object.assign([], state, action.polls);
     default:
