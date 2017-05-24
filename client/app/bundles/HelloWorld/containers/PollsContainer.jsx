e34bc427755b908ca9f6accee2fc00308b16b37a
jasmo2 <jasmo2.com@gmail.com>
2017-05-24 16:24:38 -0500

    Add PollsContainer
    Former-commit-id: 5950292f06d43933dcc7621242895cb2ec36eb6d

--- client/app/bundles/HelloWorld/containers/PollsContainer.jsx
+++ client/app/bundles/HelloWorld/containers/PollsContainer.jsx
@@ -1,13 +1,30 @@
 // Simple example of a React "smart" component
-
+import React, {Component} from 'react';
 import { connect } from 'react-redux';
-import HelloWorld from '../components/HelloWorld';
-import * as actions from '../actions/helloWorldActionCreators';
+import Polls from '../components/Polls';
+import { getPolls } from '../actions';
 
 // Which part of the Redux global state does our component want to receive as props?
-const mapStateToProps = (state) => ({ name: state.name });
+const mapStateToProps = (state) => {
+    const { polls } = state;
+    return { polls };
+};
 
+const mapDispatchToProps = {
+    getPolls
+}
+class PollsContainer extends Component {
+
+    componentWillMount(){
+        this.props.getPolls();
+    }
+
+    render() {
+        const { polls } = this.props;
+        return <Polls polls={polls} />
+    }
+}
 // Don't forget to actually use connect!
-// Note that we don't export HelloWorld, but the redux "connected" version of it.
+// Note that we don't export Polls, but the redux "connected" version of it.
 // See https://github.com/reactjs/react-redux/blob/master/docs/api.md#examples
-export default connect(mapStateToProps, actions)(HelloWorld);
+export default connect(mapStateToProps, mapDispatchToProps)(PollsContainer);
