ac2854f73202b0314f4ecad7234fd2761440b141
jasmo2 <jasmo2.com@gmail.com>
2017-05-24 16:21:57 -0500

    Apply middleware thunk
    Former-commit-id: 0c38a2665eaf54776cd7507f0654518ea12c24cc

--- client/app/bundles/HelloWorld/store/helloWorldStore.jsx
+++
@@ -1,8 +0,0 @@
-import { createStore } from 'redux';
-import helloWorldReducer from '../reducers';
-
-const configureStore = (railsProps) => (
-  createStore(helloWorldReducer, railsProps)
-);
-
-export default configureStore;
