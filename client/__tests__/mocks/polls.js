b27a615988e68a39e7a2f1f7a02e3952fe6133eb
jasmo2 <jasmo2.com@gmail.com>
2017-05-25 11:20:48 -0500

    add polls response mock
    Former-commit-id: 6f53e077a699866f746d8d691e8d8e8effe6b995

---
+++ client/__tests__/mocks/polls.js
@@ -0,0 +1,8 @@
+export const pollsMock =  [
+    {
+        "already_voted": false, "description": "El alex y sus propuestas", "id": 1, "title": "El Alex", "type": null
+    },
+    {
+        "already_voted": false, "description": "description", "id": 2, "title": "James", "type": null
+    }
+]
