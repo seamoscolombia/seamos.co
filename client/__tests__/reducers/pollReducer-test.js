50bb62a00b3f16b542a66a93d05df8adfb14f23e
jasmo2 <jasmo2.com@gmail.com>
2017-05-25 14:37:37 -0500

    fix references
    Former-commit-id: 2bb799a9ccaec293b901ee7bd7e64dc5db9c6628

--- client/__tests__/reducers/pollReducer-test.js
+++ client/__tests__/reducers/pollReducer-test.js
@@ -1,5 +1,5 @@
-import pollReducer from '../../app/bundles/Seamos/reducers/pollReducer'
-import { SET_POLL, UPDATE_POLL, VALIDATE_ACTUAL_POLL } from '../../app/bundles/Seamo/constants/';
+import pollReducer from '../../app/bundles/Seamos/reducers/pollReducer';
+import { SET_POLL, UPDATE_POLL, VALIDATE_ACTUAL_POLL } from '../../app/bundles/Seamos/constants';
 
 describe('pollReducers test',()=>{
     it(`+++ reducer for ${SET_POLL}`, () => {
