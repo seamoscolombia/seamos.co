99bde31e139f613ef2de9965c4b3fab7ad1e51aa
jasmo2 <jasmo2.com@gmail.com>
2017-05-30 11:31:33 -0500

    configure main component to acept router
    Former-commit-id: 865992f780e7f5a24bb9af7fcf052473a108106f

--- client/app/bundles/Seamos/startup/Seamos.jsx
+++ client/app/bundles/Seamos/startup/Seamos.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
+import AppRouter from './router';
 import configureStore from '../store';
-import PollsContainer from '../containers/pollsContainer';
+
 
 // See documentation for https://github.com/reactjs/react-redux.
 // This is how you get props from the Rails view into the redux store.
 // This code here binds your smart component to the redux store.
 // railsContext provides contextual information especially useful for server rendering, such as
 // knowing the locale. See the React on Rails documentation for more info on the railsContext
-const PollsApp = (props, _railsContext) => (
+const SeamosApp = (props) => (
   <Provider store={configureStore(props)}>
-    <PollsContainer />
+    <AppRouter />
   </Provider>
 );
 
-export default PollsApp;
+export default SeamosApp;
