b1392ac228786d0a97cc2dafc3ec3571849e0fd3
jasmo2 <jasmo2.com@gmail.com>
2017-05-30 16:01:59 -0500

    add react-facebook-login package && add container
    Former-commit-id: 3b0c5bc4b4e3ae70632a11a39c0d7d0dff0997ed

---
+++ client/app/bundles/Seamos/containers/facebookLoginContainer.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import FacebookLogin from 'react-facebook-login';
+
+import { setUser } from '../actions';
+
+// const mapStateToProps = (state) => { };
+
+const mapDispatchToProps = { setUser };
+
+class FacebookLoginContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.responseFacebook = this.responseFacebook.bind(this);
+  }
+
+  responseFacebook(response) {
+    console.log(response);
+  }
+
+  render() {
+    return (
+      <FacebookLogin
+        appId="1790047761280618"
+        autoLoad
+        fields="id,first_name,last_name,email,picture"
+        callback={this.responseFacebook}
+        cssClass="my-facebook-button-class"
+      />
+    );
+  }
+}
+
+export default connect(null, mapDispatchToProps)(FacebookLoginContainer);
