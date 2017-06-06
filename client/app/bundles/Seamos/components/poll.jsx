522c271f048d3619bf0a86daad3bab9ffa09a89c
jasmo2 <jasmo2.com@gmail.com>
2017-06-06 12:45:02 -0500

    receive tag obj
    Former-commit-id: 6e6ae2ffe552ca05c1d64ab887f92f883fd2692d

--- client/app/bundles/Seamos/components/poll.jsx
+++ client/app/bundles/Seamos/components/poll.jsx
@@ -1,33 +1,39 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const Poll = ({ title, description, poll_image, vote_count, remaining }) => (
-  <div id='poll-component'>
-    <div className='poll-image-container'>
-      <img alt="poll" src={poll_image} />
-    </div>
-    <div className='color-separator' style={{"backgroundColor": "#333"}} />
-    <div className='poll-infos-container'>
-      <div className='poll-info'>
-        <div className='poll-title'>
-          {title}
+function getDays(remaining) {
+  return ((remaining / 3600) / 24);
+}
+const Poll = ({ title, description, poll_image, vote_count, remaining, tag }) => {
+  const { tag_image, tag_icon, tag_color, tag_name } = tag;
+  return (
+    <div id='poll-component'>
+      <div className='poll-image-container'>
+        <img alt="poll" src={poll_image} />
+      </div>
+      <div className='color-separator' style={{ backgroundColor: tag_color }} />
+      <div className='poll-infos-container'>
+        <div className='poll-info'>
+          <div className='poll-title'>
+            {title}
+          </div>
+          <div className='poll-description'>
+            {description}
+          </div>
         </div>
-        <div className='poll-description'>
-          {description}
+        <div className='poll-details'>
+          {vote_count} participaciones <span> | quedan {getDays(remaining)} días</span>
         </div>
       </div>
-      <div className='poll-details'>
-        {vote_count} participaciones  <span> | quedan {remaining / 3600 / 24} días</span>
-      </div>
     </div>
-  </div>
-
-);
+  );
+}
 
 Poll.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   poll_image: PropTypes.string.isRequired,
+  tag: PropTypes.tag.isRequired,
   vote_count: PropTypes.number.isRequired,
   remaining: PropTypes.number.isRequired
 };
