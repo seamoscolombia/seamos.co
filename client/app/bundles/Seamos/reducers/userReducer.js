5fa7c1520b1345d7e8983775869491276e8f0131
Alexander Quiceno <yonnyquiceno@gmail.com>
2017-06-09 14:46:27 -0500

    Bugfix
    Former-commit-id: a3b7bd92d65c9a2388f3e892dc9c3727e5a9b038

--- client/app/bundles/Seamos/reducers/userReducer.js
+++ client/app/bundles/Seamos/reducers/userReducer.js
@@ -1,11 +1,11 @@
-import { 
+import {
   DELETE_TAGS_ON_USER, RESET_SESSION, SET_USER, ADD_TAGS_ON_USER
 } from '../constants';
 
 const INITIAL_STATE = localStorage.getItem('user') ? //eslint-disable-line
   JSON.parse(localStorage.getItem('user')) //eslint-disable-line
-  : { }; 
-  
+  : { };
+
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case RESET_SESSION: {
@@ -24,13 +24,13 @@ export default (state = INITIAL_STATE, action) => {
       return user;
     }
     case DELETE_TAGS_ON_USER: {
-      const tagIndex = state.tags.findIndex(tag => tag.id === action.tag.id);
+      const tagIndex = state.tags.findIndex(tag => tag.tag_id === action.tag.tag_id);
       state.tags.splice(tagIndex, 1);
       const user = Object.assign({}, state);
       localStorage.setItem('user', JSON.stringify(user)); //eslint-disable-line
       return user;
     }
-    
+
     default: return state;
   }
 };
