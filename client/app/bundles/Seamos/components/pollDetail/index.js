939d96e7b3e036cfc95c3697c5884ce849b5d57c
Alexander Quiceno <yonnyquiceno@gmail.com>
2017-06-20 15:29:50 -0500

    minor fix
    Former-commit-id: 79f1e65c8a638d4569c5e09faa53f72a2111155d

--- client/app/bundles/Seamos/components/pollDetail/index.js
+++ client/app/bundles/Seamos/components/pollDetail/index.js
@@ -46,10 +46,10 @@ function votedButton(pollType, voteTypes, vote_count) {
   }
 }
 
-function getPicture(politician) {
-  if (politician.picture !== 'null') {
-    return politician.picture
-  }
+function getPicture(picture) {
+  // if (picture) {
+  //   return picture
+  // }
   return 'https://developers.google.com/experts/img/user/user-default.png';
 }
 
@@ -69,7 +69,7 @@ const PollDetail = ({
       </header>
       <section id='politician' className='row'>
           <img
-            src={getPicture(politician)}
+            src={getPicture(politician.picture)}
             role='presentation'
             alt='politician'
           />
