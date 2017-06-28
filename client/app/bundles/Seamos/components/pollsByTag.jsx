f22df88a53f21d34ed4b8513f6521d091c63d754
jasmo2 <jasmo2.com@gmail.com>
2017-06-27 22:21:24 -0500

    share poll by tag
    Former-commit-id: 660bb06e222d0425fb706828c46099f0861ea6c8

--- client/app/bundles/Seamos/components/pollsByTag.jsx
+++ client/app/bundles/Seamos/components/pollsByTag.jsx
@@ -1,6 +1,16 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { ShareButtons, generateShareIcon } from 'react-share';
 import Polls from './polls';
+import { URL } from '../constants';
+
+const shareUrl = URL === 'http://localhost:3000/' ? 'seamos.herokuapp.com' : `${URL}${window.location.hash}`;
+const shareDescription = 'somos una organización que ayuda a la democracía';
+const { FacebookShareButton } = ShareButtons;
+const FacebookIcon = generateShareIcon('facebook');
+
+const imgUrl = 'https://cdn.keycdn.com/img/cdn-network.svg';
+const title = 'Seamos pagina home';
 
 const PollsByTag = (props) => {
   const { image, icon, color, name } = props.tag;
@@ -10,6 +20,20 @@ const PollsByTag = (props) => {
         <div className='icon-name-wrapper'>
           <div className='tag-icon-container'>
             <img className='tag-icon' alt='tag icon' src={icon} />
+            <FacebookShareButton
+              url={shareUrl}
+              title={title}
+              picture={imgUrl}
+              description={shareDescription}
+              className="network__share-button"
+            >
+              <FacebookIcon
+                size={32}
+                round
+              >
+                Compartir
+            </ FacebookIcon>
+            </FacebookShareButton>
           </div>
           <div className='tag-name'>
             <h1> {name}</h1>
@@ -17,7 +41,7 @@ const PollsByTag = (props) => {
         </div>
       </div>
       <div className='polls-box'>
-        { props.polls.length !== 0 ?
+        {props.polls.length !== 0 ?
           <Polls {...props} /> :
           <h3> Por el momento no hay propuestas para este tema </h3>
         }
