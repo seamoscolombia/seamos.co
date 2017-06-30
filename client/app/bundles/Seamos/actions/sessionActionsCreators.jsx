f8f019f17e83a4949e21aa133a514d48a288983f
Jaime Molina <js.molina13@uniandes.edu.co>
2017-06-30 12:22:06 -0400

    change citizen logout URL
    Former-commit-id: 20a0b61b563007900144d88b002190a98c7eb0f5

--- client/app/bundles/Seamos/actions/sessionActionsCreators.jsx
+++ client/app/bundles/Seamos/actions/sessionActionsCreators.jsx
@@ -10,15 +10,8 @@ export const setSession = (authenticityToken) => ({
 export const resetUserSession = () => ({ type: RESET_SESSION });
 
 export const resetSession = () => dispatch => (
-  axios.delete(`${URL}/sessions.json`)
-  .then(() => {
-    // debugger
-    // window.FB.logout(function(response) {
-    //   debugger
-
-    // });
-    dispatch(resetUserSession());
-  })
+  axios.delete(`${URL}/destroy_facebook_session.json`)
+  .then(() => dispatch(resetUserSession()))
   .catch(e => {
     alert('Ha ocurrido un error por favor reporta a nuestro equipo'); //eslint-disable-line
   })
