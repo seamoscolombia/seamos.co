0a58cd8168731918682036f72efd0427185f2ed1
Alexander Quiceno <yonnyquiceno@gmail.com>
2017-08-11 02:21:06 -0500

    show no polls for selected tags advice only on homepage
    Former-commit-id: 8573579b58ffedd9262c19814b56027db9194337

--- client/app/bundles/Seamos/components/polls.jsx
+++ client/app/bundles/Seamos/components/polls.jsx
@@ -5,11 +5,14 @@ import Poll from './poll';
 
 const Polls = ({ polls, tag }) => (
   <div className='polls-wrapper'>
-    { polls.length !== 0 ? polls.map(poll => (
-        <Poll {...poll} key={poll.id} tag={tag} />
-      )) :
+    { polls.length === 0 && window.location.hash.split('/')[1] === '' ?
       <h3> En el momento no tenemos propuestas abiertas a votación
-      en tus temas de interés, recuerda seleccionarlos en tu perfil. </h3>
+      en tus temas de interés, recuerda seleccionarlos en tu perfil. </h3> :
+      polls.map(
+          poll => (
+            <Poll {...poll} key={poll.id} tag={tag} />
+                  )
+               )
     }
   </div>
 );
