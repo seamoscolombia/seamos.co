6c684cb37ac6780dda5bd65ba34ed393fb2b13fd
Alexander Quiceno <yonnyquiceno@gmail.com>
2017-08-15 15:56:59 -0500

    initial progress on new footer
    Former-commit-id: 2fe4221f952c14427fbc329cf3c884cdd8872094

--- client/app/bundles/Seamos/components/footer.jsx
+++ client/app/bundles/Seamos/components/footer.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 
 const Footer = () => (
   <div id='client-footer'>
-    <div className='disclaimer-container'>
-      <span className='disclaimer'>Copyright 2017 SeamOS Democracia Digital</span>
-    </div>
     <div className='logo-container'>
       <div className='logo-background'>
         <div className='logo'>
           <br />
+          <br />
         </div>
       </div>
+    </div>
+    <div className='disclaimer-container'>
+      <span className='disclaimer'>Copyright 2017 SeamOS Democracia Digital</span>
     </div>
   </div>
 );
