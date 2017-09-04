30a8b93e6e1b896afb252199ba6b2a67597cd974
Alexander Quiceno <yonnyquiceno@gmail.com>
2017-09-04 18:30:33 -0500

    Validate user session befor navbar mount
    Former-commit-id: 44b5a8df94e314937da4c5b5d18587d7778374f7

--- client/app/bundles/Seamos/components/navbar.jsx
+++ client/app/bundles/Seamos/components/navbar.jsx
@@ -48,7 +48,7 @@ const Navbar = ({ session, user }) => (
           <div id='brand-logo' />
         </Link>
         <div className='hide-on-desktop only-photo'>
-          {session.authenticityToken && Object.keys(user).length !== 0 ?
+          {session.logged && Object.keys(user).length !== 0 ?
             profile() : <li key='11'> <FacebookLogin fbclassName='nav-fb' fbText='' /> </li>
           }
         </div>
@@ -70,7 +70,7 @@ const Navbar = ({ session, user }) => (
         <nav className='navbar-collapse' role='navigation'>
           <ul className='nav navbar-nav navbar-right navbar-options'>
             <div className='hide-on-desktop'>
-              {session.authenticityToken && Object.keys(user).length !== 0 ?
+              {session.logged && Object.keys(user).length !== 0 ?
                 profile() : <li key='5'> <FacebookLogin fbclassName='nav-fb' fbText='REGÍSTRATE' /> </li>
               }
             </div>
@@ -86,88 +86,12 @@ const Navbar = ({ session, user }) => (
               </a>
             </li>
             <span className='circle-separator'> &#9679; </span>
-            {/*
-              <li key='8' className='dropdown hide-on-mobile'>
-                <a> Siguenos </a>
-                <div className='dropdown-content'>
-                  <a
-                    className='social-icon facebook-icon'
-                    href="https://www.facebook.com/seamoscolombia/"
-                    target='_blank'
-                    rel='noopener noreferrer'
-                  >
-                    <br />
-                  </a>
-                  <a
-                    className='social-icon twitter-icon'
-                    href="https://twitter.com/seamos"
-                    target='_blank'
-                    rel='noopener noreferrer'
-                  >
-                    <br />
-                  </a>
-                  <a
-                    className='social-icon instagram-icon'
-                    href="https://www.instagram.com/seamoscolombia/"
-                    target='_blank'
-                    rel='noopener noreferrer'
-                  >
-                    <br />
-                  </a>
-                  <a
-                    className='social-icon youtube-icon'
-                    href="https://www.youtube.com/channel/UCC8EUjRTWhzELH3_iypQQ8w"
-                    target='_blank'
-                    rel='noopener noreferrer'
-                  >
-                    <br />
-                  </a>
-                </div>
-              </li>
-            <li key='10' className='hide-on-desktop sidebar-social'>
-              <a style={{textAlign: 'center'}}> Siguenos </a>
-              <div className="social-icons-wrapper">
-                <a
-                  className='social-icon facebook-icon'
-                  href="https://www.facebook.com/seamoscolombia/"
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <br />
-                </a>
-                <a
-                  className='social-icon twitter-icon'
-                  href="https://twitter.com/seamos"
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <br />
-                </a>
-                <a
-                  className='social-icon instagram-icon'
-                  href="https://www.instagram.com/seamoscolombia/"
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <br />
-                </a>
-                <a
-                  className='social-icon youtube-icon'
-                  href="https://www.youtube.com/channel/UCC8EUjRTWhzELH3_iypQQ8w"
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <br />
-                </a>
-              </div>
-            </li>
-          */}
             <div className='hide-on-mobile'>
-              {session.authenticityToken && Object.keys(user).length !== 0 ?
+              {session.logged && Object.keys(user).length !== 0 ?
                 profile() : <li key='11'> <FacebookLogin fbclassName='nav-fb' fbText='Regístrate' /> </li>
               }
             </div>
-            {session.authenticityToken && Object.keys(user).length !== 0 ?
+            {session.logged && Object.keys(user).length !== 0 ?
               logout() : <span />
             }
           </ul>
--- client/app/bundles/Seamos/containers/navbarContainer.jsx
+++ client/app/bundles/Seamos/containers/navbarContainer.jsx
@@ -3,14 +3,22 @@ import React, { Component } from 'react';
 import jQuery from 'jquery';
 import { connect } from 'react-redux';
 import Navbar from '../components/navbar';
+import { validateSession } from '../actions';
 
 const mapStateToProps = (state) => {
     const { session, user } = state;
     return { session, user };
 };
 
+const mapDispatchToProps = { validateSession };
 
 class NavbarContainer extends Component {
+
+    componentWillMount() {
+      console.log('validada ');
+      this.props.validateSession();
+    }
+
     componentDidMount() {
         const sideslider = jQuery('[data-toggle=collapse-side]');
         const sel = sideslider.attr('data-target');
@@ -22,7 +30,7 @@ class NavbarContainer extends Component {
             jQuery(sel).toggleClass('in');
             jQuery(sel2).toggleClass('out');
         });
-        body.addEventListener('click', () => { 
+        body.addEventListener('click', () => {
             jQuery(sel).removeClass('out');
             jQuery(sel).addClass('in');
         });
@@ -35,4 +43,4 @@ class NavbarContainer extends Component {
 // Don't forget to actually use connect!
 // Note that we don't export Navbar, but the redux "connected" version of it.
 // See https://github.com/reactjs/react-redux/blob/master/docs/api.md#examples
-export default connect(mapStateToProps)(NavbarContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(NavbarContainer);
