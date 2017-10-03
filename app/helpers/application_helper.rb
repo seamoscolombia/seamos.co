c7216b835aab7dd5c8a35ca823e6eb3422376d91
Alexander Quiceno <yonnyquiceno@gmail.com>
2017-10-03 12:51:54 -0500

    remove meta tags rendering second approach
    Former-commit-id: 8b4da6288b544a75bb32e049415e2825a355ff5a

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,3 @@
 module ApplicationHelper
-  def meta_tag(tag, text)
-    session[:"meta_#{tag}"] = text
-  end
 
-  def yield_meta_tag(tag, default_text='')
-    session[:"meta_#{tag}"] ? session[:"meta_#{tag}"] : default_text
-  end
 end
