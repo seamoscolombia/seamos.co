b4fb9fb390fc87d13ae6c26f186ea327c507d3a9
Alexander Quiceno <yonnyquiceno@gmail.com>
2018-02-26 15:13:31 -0500

    Style updates for politician profile and poll deail views
    Former-commit-id: ca3ee1c021bbe669fab3a2fb7a68a55b096ee685

--- client/app/bundles/Seamos/components/pollDetail/index.js
+++ client/app/bundles/Seamos/components/pollDetail/index.js
@@ -173,11 +173,6 @@ const PollDetail = ({
           <div className="tag-name"> {tag.name} </div>
         </div>
         <div className='container'>
-          <header className='row'>
-            <p className='col-sm-12 poll-title'>
-              {title}
-            </p>
-          </header>
           <section id='politician' className='row'>
             <img
               src={getPicture(politician)}
@@ -193,8 +188,13 @@ const PollDetail = ({
               <div id='org'> {politician.organization} </div>
             </div>
           </section>
+          <header className='row'>
+            <p className='col-sm-12 poll-title'>
+              {title}
+            </p>
+          </header>
           <div className='share-wrapper'>
-            <span className='share-this'> COMPARTIR: </span>
+            <span className='share-this hide-on-mobile'> COMPARTIR: </span>
               <FacebookShareButton
                 url={`${URL}/client/polls/${id}`}
                 className="network__share-button"
@@ -255,7 +255,6 @@ const PollDetail = ({
               <div className="row">
                 <div className="col-sm-12">
                   <div className="row">
-                    <div className='summary'> {summary} </div>
                     <div className="col-xs-12 col-sm-6 buttons-wrapper">
                       {user_already_voted || remaining <= 0 ?
                         votedButton(type, vote_types, vote_count, remaining <= 0 ) :
@@ -266,6 +265,7 @@ const PollDetail = ({
                         { getDays(remaining) }
                     </div>
                   </div>
+                  <div className='summary'> {summary} </div>
                 </div>
                 <div className="poll-description-container col-sm-12">
                   <div className="poll-static-title"> La Propuesta: </div>
