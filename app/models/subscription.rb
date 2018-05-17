6c0393a78d83de8880a91cd20e1fdd061458ce99
Alexander Quiceno <yonnyquiceno@gmail.com>
2018-05-17 16:58:09 -0500

    Changes Gemfile ruby version to 2.3.6
    Former-commit-id: 8460c7c7f648efacbf33a3ea57e0753400fb47d1

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,5 +1,5 @@
 class Subscription < ApplicationRecord
-  validates_uniqueness_of :email, message: "Ya te has suscrito previamente"
+  validates_uniqueness_of :email, :scope => [:email_list_id], message: "Ya te has suscrito previamente"
   validates  :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/,
                      :message => "Formato de email inválido"}
 end
