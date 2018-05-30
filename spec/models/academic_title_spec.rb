14252acecf6eb62a759d3ef69d937c107c8a1cff
Joshua <joshuaprpich@gmail.com>
2018-05-29 21:51:57 -0500

    Add relations test to AcademicTitle model
    Former-commit-id: 6f02968eb0552ee2c2c8d1e017c7a12f6ee1174a

--- spec/models/academic_title_spec.rb
+++ spec/models/academic_title_spec.rb
@@ -15,5 +15,7 @@
 require 'rails_helper'
 
 RSpec.describe AcademicTitle, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relations' do
+    it { should belong_to(:user) }
+  end
 end
