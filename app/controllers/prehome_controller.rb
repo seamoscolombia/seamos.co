cbae87e02e4041c0f4d3a15a2a6d01e0b027aba4
Joshua <joshuaprpich@gmail.com>
2018-06-06 14:46:58 -0500

    separate navbars
    Former-commit-id: 99ab76223f3391d944a0950452eb3e0e815c27dd

--- app/controllers/prehome_controller.rb
+++ app/controllers/prehome_controller.rb
@@ -1,5 +1,10 @@
 class PrehomeController < ApplicationController
-  def index
+  before_action :validate_user, only: :index
+  
+  def index    
+  end
+
+  def validate_user 
     if current_user && (current_user.administrador? || current_user.politico? )
       redirect_to admin_homepage_path
     end
