7a930fd7165b691162d10b09c1da2247453f8113
Joshua <joshuaprpich@gmail.com>
2018-06-15 08:15:14 -0500

    start showing only two proposals
    Former-commit-id: 552656dbee6754bab71898ea7404802dca51945e

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @tags = Tag.joins(:polls).uniq
-    @polls = Poll.includes(:user, :tags)
+    @polls = Poll.includes(:user, :tags, :votes).sort_by {|poll| poll.vote_count}.last(2)
   end
 end
