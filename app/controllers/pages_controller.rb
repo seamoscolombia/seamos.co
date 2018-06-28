54cea31aca9f0bebbf19d8b05ebd7e60daba2562
Joshua <joshuaprpich@gmail.com>
2018-06-27 22:26:26 -0500

    delete method
    Former-commit-id: 57f3635893f39ec4804e431f9371564fbd0c4597

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,6 @@
 class PagesController < ApplicationController
-  before_action :validate_user, only: :index
   
-  def politicians    
+  def politicians  
   end
-
-  def validate_user 
-    if current_user && (current_user.administrador? || current_user.politico? )
-      redirect_to admin_homepage_path
-    end
-  end
+  
 end
