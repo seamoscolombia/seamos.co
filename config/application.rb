5f4798036bed9432b8155a49508850c0aa667dbc
Joshua <joshuaprpich@gmail.com>
2018-07-04 15:43:17 -0500

    fix application.rb
    Former-commit-id: b46addd7fe978045eed601692070c8510d96005e

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-ENV.update YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
-
 module Seamos
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -22,5 +20,14 @@ module Seamos
     config.i18n.default_locale = :es
     config.time_zone = 'Bogota'
     Koala.config.api_version = 'v2.0'
+
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'application.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
   end
 end
+
+
