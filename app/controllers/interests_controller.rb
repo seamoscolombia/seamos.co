686c40688b01efd7da825769afa6bd87cf72b7f9
Alexander Quiceno <yonnyquiceno@gmail.com>
2018-07-04 22:15:59 -0500

    add before filter to ensure correct access level
    Former-commit-id: 3d32ba5f7f60228409c5cba467f0ea5b05810c9e

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -1,4 +1,5 @@
 class InterestsController < ApplicationController
+  before_action :verify_user
 
   def association
     @interest = Interest.where('user_id=? AND tag_id=?',params[:user_id], params[:tag_id]).first
@@ -9,12 +10,21 @@ class InterestsController < ApplicationController
       @interest = Interest.new(
         user_id: params[:user_id],
         tag_id: params[:tag_id]
-      ) 
+      )
       if @interest.save
         render json: { message: "interest succesfully created", interest: @interest, status: :created}
       else
         render json: @interest.errors, status: :unprocessable_entity
       end
     end
-  end 
+  end
+
+  private
+
+  def verify_user
+    unless params[:user_id] && current_user.id.to_s == params[:user_id]
+      flash[:warning] = "Acción no permitida"
+      redirect_to root_path
+    end
+  end
 end
