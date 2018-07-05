47fcd26c8246b6c0d913037e6f79aff18c2b1356
Alexander Quiceno <yonnyquiceno@gmail.com>
2018-07-04 22:34:18 -0500

    fix failing tests
    Former-commit-id: f7b6fcb6a783f047498d697224cc7e50891e0e9e

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, prepend: true
+
+  def validate_session    
+    redirect_to root_path if current_user.nil?
+  end
   
   def validate_superadmin
     flash[:warning] = "Primero debes acceder como administrador"
