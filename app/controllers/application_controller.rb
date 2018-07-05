471594b40253cfc2148af6530f4cf74e2893ced7
Alexander Quiceno <yonnyquiceno@gmail.com>
2018-07-04 22:26:08 -0500

    Aplication controller revision
    Former-commit-id: 723a8658b0cf496108c615e8beec0e1eda4c19de

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, prepend: true
-  include SessionsHelper
-
-  def validate_session
-    if current_user.nil?
-      respond_to do |format|
-        format.html { redirect_to root_path }
-        format.json { render json: {}, status: :unauthorized }
-      end
-    end
-  end
   
   def validate_superadmin
     flash[:warning] = "Primero debes acceder como administrador"
