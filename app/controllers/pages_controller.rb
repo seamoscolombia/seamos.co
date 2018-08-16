7381a6ad80ba1af6bc0df55f4c9393065be9401b
Alexander Quiceno <yonnyquiceno@gmail.com>
2018-08-15 20:07:10 -0500

    initial progress on diplomat page
    Former-commit-id: a29328a1c01b065482243a4965e3fcaa99d4186a

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,12 @@
 class PagesController < ApplicationController
-  
-  def politicians  
+
+  def politicians
     @politicians = JSON.parse(File.read("app/helpers/politicians.json"))
   end
-  
+
   def terms
+  end
+
+  def diplomat
   end
 end
