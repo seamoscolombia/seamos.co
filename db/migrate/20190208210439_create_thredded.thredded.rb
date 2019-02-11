7fd9d87e92b75e5de0a65be360975ab131b267c1
Alexander Quiceno <yonnyquiceno@gmail.com>
2019-02-11 10:38:26 -0500

    Add up and down methods to thredded migrations in order to properly migrate and revert thredded tables

--- db/migrate/20190208210439_create_thredded.thredded.rb
+++ db/migrate/20190208210439_create_thredded.thredded.rb
@@ -6,7 +6,7 @@ require 'thredded/base_migration'
 # rubocop:disable Metrics/ClassLength
 # rubocop:disable Metrics/MethodLength
 class CreateThredded < Thredded::BaseMigration
-  def change
+  def up
     unless table_exists?(:friendly_id_slugs)
       # The user might have installed FriendlyId separately already.
       create_table :friendly_id_slugs do |t|
@@ -271,6 +271,33 @@ class CreateThredded < Thredded::BaseMigration
                     Thredded.user_class.table_name, column: :user_id, on_delete: :cascade
     add_foreign_key :thredded_user_post_notifications,
                     :thredded_posts, column: :post_id, on_delete: :cascade
+  end
+  def down
+    remove_foreign_key :thredded_messageboard_users, :thredded_messageboards
+    remove_foreign_key :thredded_messageboard_users, :thredded_user_details
+    remove_foreign_key :thredded_user_post_notifications, :thredded_posts
+    drop_table :friendly_id_slugs
+    drop_table :thredded_categories
+    drop_table :thredded_messageboards
+    drop_table :thredded_posts
+    drop_table :thredded_private_posts
+    drop_table :thredded_private_topics
+    drop_table :thredded_private_users
+    drop_table :thredded_topic_categories
+    drop_table :thredded_topics
+    drop_table :thredded_user_details
+    drop_table :thredded_messageboard_users
+    drop_table :thredded_user_preferences
+    drop_table :thredded_user_messageboard_preferences
+    drop_table :thredded_messageboard_groups
+    drop_table :thredded_user_topic_follows
+    drop_table :thredded_post_moderation_records
+    drop_table :thredded_notifications_for_private_topics
+    drop_table :thredded_notifications_for_followed_topics
+    drop_table :thredded_messageboard_notifications_for_followed_topics
+    drop_table :thredded_user_post_notifications
+    drop_table :thredded_user_topic_read_states
+    drop_table :thredded_user_private_topic_read_states
   end
 end
 # rubocop:enable Metrics/MethodLength
