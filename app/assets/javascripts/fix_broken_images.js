f63aababeb9adae6c497c666a4b177378eca36bd
Alexander Quiceno <yonnyquiceno@gmail.com>
2019-02-21 13:12:08 -0500

    Uses tooltip to notify user that must be logged in in order to vote at forum threads and posts

--- app/assets/javascripts/fix_broken_images.js
+++ app/assets/javascripts/fix_broken_images.js
@@ -5,10 +5,8 @@ $(document).ready(function () {
       var i = 0,
       l = img.length;
       for (; i < l; i++) {
-        console.log(img[i].naturalHeight);
 
         if (img[i].naturalWidth === 0) {
-          console.log('image broken');
           img[i].src = 'https://s3.amazonaws.com/misc-assets-seamos/defaultavatar.jpg';
         }
       }
