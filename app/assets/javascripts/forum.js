6ff8d6cf729238c6a544423db9dc72c99fff5037
Alexander Quiceno <yonnyquiceno@gmail.com>
2019-03-14 08:49:35 -0500

    Forum mobile version fixes

--- app/assets/javascripts/forum.js
+++ app/assets/javascripts/forum.js
@@ -8,17 +8,19 @@ $(document).ready(function () {
     }
   });
 
-  var textarea = $('#forum_post_form_wrapper').find('#forum_post_body')
-  var submit_button = $('#forum_post_form_wrapper').find('button[type="submit"]')
+  $("[data-behavior~=comment-form]").each(function(){
+    var textarea = $(this).find('#forum_post_body')
+    var submit_button = $(this).find('button[type="submit"]')
 
-  submit_button.prop('disabled', true);
+    submit_button.prop('disabled', true);
 
-  textarea.on("keyup", function() {
+    textarea.on("keyup", function() {
 
-    if(textarea.val() == ''){
-      submit_button.prop('disabled' , true);
-    } else {
-      submit_button.prop('disabled' , false);
-    }
+      if(textarea.val() == ''){
+        submit_button.prop('disabled' , true);
+      } else {
+        submit_button.prop('disabled' , false);
+      }
+    })
   })
 })
