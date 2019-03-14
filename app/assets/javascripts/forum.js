7a6cbee9ad107f54422783f366e742ec6061cfc2
Alexander Quiceno <yonnyquiceno@gmail.com>
2019-03-13 22:18:29 -0500

    Several fixes for forum post form

--- app/assets/javascripts/forum.js
+++ app/assets/javascripts/forum.js
@@ -1,3 +1,24 @@
 $(document).ready(function () {
   $( 'p:empty' ).remove();
+
+  $("form").keypress(function(e) {
+    //Enter key
+    if (e.which == 13 && $(this).find('textarea').val() == '') {
+      return false;
+    }
+  });
+
+  var textarea = $('#forum_post_form_wrapper').find('#forum_post_body')
+  var submit_button = $('#forum_post_form_wrapper').find('button[type="submit"]')
+
+  submit_button.prop('disabled', true);
+
+  textarea.on("keyup", function() {
+
+    if(textarea.val() == ''){
+      submit_button.prop('disabled' , true);
+    } else {
+      submit_button.prop('disabled' , false);
+    }
+  })
 })
