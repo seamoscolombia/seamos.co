d4263436facd69215179d7e2656dc0582e7adb14
Alexander Quiceno <yonnyquiceno@gmail.com>
2019-03-15 16:37:01 -0500

    blog widget completed, added rest of html schema

--- app/controllers/prehome_controller.rb
+++ app/controllers/prehome_controller.rb
@@ -1,9 +1,11 @@
-class PrehomeController < ApplicationController  
+class PrehomeController < ApplicationController
   include ApplicationHelper
-  
+
   before_action :redirect_admin_or_politician_users
 
   def index
+    @blog_posts = HTTParty.get('https://seamosit.github.io/posts.json')
+    @politicians = JSON.parse(File.read("app/helpers/politicians.json"))
   end
 
   private
